4cc87aef2af9ba9a61a8e6f5807a84adcf7a835c
Joeli Pikkarainen <H8897@LABRANET.JAMK.FI>
2016-10-25 12:12:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/h8897/cards/Employee.java
@@ -0,0 +1,21 @@
+package com.example.h8897.cards;
+
+/**
+ * Created by H8897 on 25.10.2016.
+ */
+
+public class Employee {
+    String name;
+    String position;
+    String email;
+    String phone;
+    int photoId;
+
+    public Employee(String name, String position, String email, String phone, int photoId) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.photoId = photoId;
+    }
+}
